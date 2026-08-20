a70bfd80c4a23943df924a4144fc19049d3b2d75
priyanshu-sarjan <137197045+priyanshu-sarjan@users.noreply.github.com>
2026-08-20 18:46:44 +0530

    feat: initialize project configuration, vite, typescript, and dependencies

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': {},
+    global: 'window',
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer/',
+    },
+  },
+});
